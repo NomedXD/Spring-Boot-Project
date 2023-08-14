b383b05e15ff83d1a09c63dd6c20ec4dbb603595
NomedXD <coovshow1@gmail.com>
2023-08-14 19:02:53 +0300

    hw_11082023 fix 1

--- src/main/java/by/teachmeskills/project/SneakersShopApplication.java
+++ src/main/java/by/teachmeskills/project/SneakersShopApplication.java
@@ -2,14 +2,10 @@ package by.teachmeskills.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 @SpringBootApplication
 public class SneakersShopApplication {
-
     public static void main(String[] args) {
-        try {
-            SpringApplication.run(SneakersShopApplication.class, args);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        SpringApplication.run(SneakersShopApplication.class, args);
     }
 }
